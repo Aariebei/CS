ebb9983f244a9b0d7eac368d5521bfdfed206930
Jeremy-Haarbosch <544355@edu.rocmn.nl>
2021-06-08 10:11:01 +0200

    6.1 finish

---
+++ h6/h6/h6/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace h6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnNext.Click += BtnNext_Click;
+            btnStart.Click += BtnStart_Click;
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            lblMax.Content = txbMax.Text;
+            lblMin.Content = txbMin.Text;
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 Window = new Window2();
+            Window.Show();
+            this.Close();
+        }
+    }
+}
