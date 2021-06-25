beede3b0b52e5624c182fbd525848cbe38a47734
Jeremy-Haarbosch <544355@edu.rocmn.nl>
2021-06-25 14:16:09 +0200

    10.5 maken

---
+++ H10/Dice/Dice/Die.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Die
+    {
+        private int _digit = 1;
+        private Random rnd = new Random();
+        public int Digit
+        {
+            get { return _digit; }
+        }
+
+        public void Roll()
+        {
+            _digit = rnd.Next(1, 6);
+        }
+
+        public void Increment()
+        {
+            _digit++;
+            if (_digit > 6)
+            {
+                _digit = 6;
+            }
+        }
+    }
+}
---
+++ H10/Dice/Dice/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Die die = new Die();
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnIncrement.Click += BtnIncrement_Click;
+            btnRoll.Click += BtnRoll_Click;
+        }
+
+        private void BtnRoll_Click(object sender, RoutedEventArgs e)
+        {
+            die.Roll();
+            lblDigit.Content = Convert.ToInt32(die.Digit);
+        }
+
+        private void BtnIncrement_Click(object sender, RoutedEventArgs e)
+        {
+            die.Increment();
+            lblDigit.Content = Convert.ToInt32(die.Digit);
+        }
+    }
+}
