689e84ff5e508e0396b931b8b5df2397ec3a5ebe
Jeremy-Haarbosch <544355@edu.rocmn.nl>
2021-06-25 13:41:17 +0200

    10.4 maken

---
+++ H10/Points/Points/Counter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points
+{
+    class Counter
+    {
+        private int _count = 0;
+        public int Count 
+        { 
+            get { return _count; }
+        }
+        public void Reset()
+        {
+            _count = 0;
+        }
+        
+        public void Increment()
+        {
+            _count++;
+        }
+
+        public void Decrement()
+        {
+            _count--;
+        }
+    }
+}
---
+++ H10/Points/Points/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Points
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Counter counter = new Counter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnDecrement.Click += BtnDecrement_Click;
+            btnIncrement.Click += BtnIncrement_Click;
+            btnReset.Click += BtnReset_Click;
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            counter.Reset();
+            lblCount.Content = counter.Count;
+        }
+
+        private void BtnIncrement_Click(object sender, RoutedEventArgs e)
+        {
+            counter.Increment();
+            lblCount.Content = counter.Count;
+        }
+
+        private void BtnDecrement_Click(object sender, RoutedEventArgs e)
+        {
+            counter.Decrement();
+            lblCount.Content = counter.Count;
+        }
+    }
+}
