a66257a8cf9e3418bd3fbee7120990a433ee1374
Jeremy-Haarbosch <544355@edu.rocmn.nl>
2021-06-25 12:49:59 +0200

    10.3 maken

---
+++ H10/Banking/Banking/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class BankAccount
+    {
+        private double _currentvalue;
+
+        public double CurrentValue
+        {
+            get { return _currentvalue;  }
+            set { _currentvalue = value;  }
+        }
+
+        public void Deposit(double amount)
+        {
+            _currentvalue += amount;
+        }
+        public void WithDraw(double amount)
+        {
+            _currentvalue -= amount;
+        }
+        public BankAccount()
+        {
+            _currentvalue = 0;
+        }
+    }
+}
---
+++ H10/Banking/Banking/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Banking
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BankAccount bankaccount = new BankAccount();
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnDesposit.Click += BtnDesposit_Click;
+            btnWithdraw.Click += BtnWithdraw_Click;
+        }
+
+        private void BtnWithdraw_Click(object sender, RoutedEventArgs e)
+        {
+            double amount = Convert.ToInt32(txbAmount.Text);
+            bankaccount.WithDraw(amount);
+            lblBalance.Content = bankaccount.CurrentValue;
+        }
+
+        private void BtnDesposit_Click(object sender, RoutedEventArgs e)
+        {
+            double amount = Convert.ToInt32(txbAmount.Text);
+            bankaccount.Deposit(amount);
+            lblBalance.Content = bankaccount.CurrentValue;
+        }
+    }
+}
